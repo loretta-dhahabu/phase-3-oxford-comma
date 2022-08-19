51b9017e337f7d642bf365e4dc5f90c01b3e9c18
loretta-dhahabu <loreydjefwa@gmail.com>
2022-08-19 17:28:49 +0300

    commit tree e6b887f02b6b299870817a0d0fa0fb67c236916b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    case array.length
+    when 1
+        array.join
+    when 2 
+        array[-1] = " and " + array[-1]
+        array.join
+    else 
+        array[-1] = "and " + array[-1]
+        array.join(", ")
+    end
 end
